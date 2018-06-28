d3af9030f3ebab18703fdddac6a7febad1c4a5f0
PedroA <pedro.alonsod@gmail.com>
2018-06-28 14:41:32 -0500

    Initial Commit

---
+++ DetectPic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectPic
+//
+//  Created by Pedro Alonso on 6/28/18.
+//  Copyright © 2018 Pedro Alonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
